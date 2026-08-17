2ee74dd915dba7e53193d49cefad8ca77eec70fa
faten-khlaif <Faten.Khlaif1@outlook.com>
2026-08-17 22:08:49 +0300

    Update Program.cs

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -34,8 +34,8 @@ class Program
         Employee emp = new Employee
         {
             Id = 202,
-            FirstName = "Kawthar",
-            LastName = "Rivera"
+            FirstName = "Faten",
+            LastName = "Khlaif"
         };
 
         // 🎭 POLYMORPHISM MAGIC:
